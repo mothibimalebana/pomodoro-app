ea32f18592f538468d39600e6893800a76b56eb0
mothibimalebana <mothibi.mm12@gmail.com>
2025-05-12 22:15:11 +0200

    still needs work

--- src/pages/Clock.jsx
+++ src/pages/Clock.jsx
@@ -4,10 +4,10 @@ const Clock = () => {
     return(
         <>
             <div className="home flex bg-[#C36868] justify-center items-center w-full  h-full">
-                <div className="clock w-8/10 h-8/10 md:w-4/10">
-                    <div className="screen">
-                        <div className="top"></div>
-                        <div className="middle bottom"></div>
+                <div className="clock flex flex-col justify-center items-start w-8/10 h-8/10 md:w-4/10">
+                    <div className="screen w-9/10 h-5/10 mt-5">
+                        <div className="top">Session</div>
+                        <div className="middle">25:00</div>
                     </div>
                     <div className="controls">
                         <div className="sessionLength"></div>
